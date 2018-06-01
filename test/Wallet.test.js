7719bef4277407c8fe4693b8c5bac4d1a7b1278f
Sky Young <sky@alexandria.io>
2018-06-01 16:58:44 -0700

    Update tests

--- test/Wallet.test.js
+++ test/Wallet.test.js
@@ -1,19 +1,20 @@
 var Wallet = require('../lib');
 
 test('Wallet can be defined from Mnemonic', () => {
-	var w = new Wallet('basket actual')
+	var w = new Wallet("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
 
-	expect(w.getSeedHex()).toBe('5cf2d4a8b0355e90295bdfc565a022a409af063d5365bb57bf74d9528f494bfa4400f53d8349b80fdae44082d7f9541e1dba2b003bcfec9d0d53781ca676651f')
+	expect(w.getSeed()).toBe('5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4')
 })
 
 test('Wallet can be defined from Seed Hex', () => {
-	var w = new Wallet('5cf2d4a8b0355e90295bdfc565a022a409af063d5365bb57bf74d9528f494bfa4400f53d8349b80fdae44082d7f9541e1dba2b003bcfec9d0d53781ca676651f')
+	var w = new Wallet('5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4')
 
-	expect(w.getMnemonic()).toBe('basket actual')
+	expect(w.getSeed()).toBe('5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4')
 })
 
 test('Wallet can be defined from BIP39 Entropy', () => {
-	var w = new Wallet('5cf2d4a8b0355e90295bdfc565a022a409af063d5365bb57bf74d9528f494bfa4400f53d8349b80fdae44082d7f9541e1dba2b003bcfec9d0d53781ca676651f')
+	var w = new Wallet('00000000000000000000000000000000')
 
-	expect(w.getMnemonic()).toBe('basket actual')
+	expect(w.getMnemonic()).toBe("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
+	expect(w.getSeed()).toBe('5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4')
 })
