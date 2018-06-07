f7451a93b8905cb36c2b4984a80b50a80f6c0ade
Sky Young <sky@alexandria.io>
2018-06-07 14:26:54 -0700

    Parse from addresses in TransactionBuilder

--- src/TransactionBuilder.js
+++ src/TransactionBuilder.js
@@ -2,21 +2,21 @@ import bitcoin from 'bitcoinjs-lib'
 import bip32 from 'bip32'
 import bip32utils from 'bip32-utils'
 import coinselect from 'coinselect'
+
+import Address from './Address'
 import { isValidAddress } from './util'
 
 const GAP_LIMIT = 20;
 
 module.exports =
 class TransactionBuilder {
-	constructor(options){
+	constructor(coin, options){
+		// From Addresses
 		this.from = [];
 
 		this.parseOptions(options);	
 	}
 	parseOptions(options){
-		// Store an array of addresses to request utxos for
-		var addrs = [];
-
 		// Check if we define what address the transaction must come from
 		if (!options.from){
 			throw new Error("From Address(es) are not defined!")
@@ -24,13 +24,21 @@ class TransactionBuilder {
 
 		if (Array.isArray(options.from)){
 			for (var addr of options.from){
-				if (isValidAddress(addr, this.coin.network)){
-					addrs.push(addr);
+				if (addr instanceof Address){
+					if (isValidAddress(addr.toBase58(), this.coin.network)){
+						this.from.push(addr)
+					}
+				} else if (isValidAddress(addr, this.coin.network)) {
+					this.from.push(new Address(addr, this.coin));
 				}
 			}
 		} else {
-			if (isValidAddress(options.from, this.coin.network)){
-				addrs.push(options.from);
+			if (options.from instanceof Address){
+				if (isValidAddress(options.from.toBase58(), this.coin.network)){
+					this.from.push(options.from)
+				}
+			} else if (isValidAddress(options.from, this.coin.network)) {
+				this.from.push(new Address(options.from, this.coin));
 			}
 		}
 	}
