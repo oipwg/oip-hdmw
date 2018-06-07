cd8b7e764cadada5426e2c9b65234030807d98bd
Sky Young <sky@alexandria.io>
2018-06-07 16:28:15 -0700

    TransactionBuilder load From and To

--- src/TransactionBuilder.js
+++ src/TransactionBuilder.js
@@ -11,34 +11,59 @@ const GAP_LIMIT = 20;
 module.exports =
 class TransactionBuilder {
 	constructor(coin, options){
-		// From Addresses
+		this.coin = coin;
+
+		// Addresses we are sending from
 		this.from = [];
+		// Addresses we want to send to & amounts
+		this.to = [];
 
 		this.parseOptions(options);	
 	}
-	parseOptions(options){
-		// Check if we define what address the transaction must come from
-		if (!options.from){
-			throw new Error("From Address(es) are not defined!")
-		}
-
-		if (Array.isArray(options.from)){
-			for (var addr of options.from){
-				if (addr instanceof Address){
-					if (isValidAddress(addr.toBase58(), this.coin.network)){
-						this.from.push(addr)
-					}
-				} else if (isValidAddress(addr, this.coin.network)) {
-					this.from.push(new Address(addr, this.coin));
-				}
+	addFrom(addr){
+		if (addr instanceof Address){
+			if (isValidAddress(addr.toBase58(), this.coin.network)){
+				this.from.push(addr)
 			}
 		} else {
-			if (options.from instanceof Address){
-				if (isValidAddress(options.from.toBase58(), this.coin.network)){
-					this.from.push(options.from)
+			throw new Error("From Address MUST BE InstanceOf Address")
+		}
+	}
+	addTo(address, amount){
+		if (isValidAddress(address, this.coin.network) && !isNaN(amount)){
+			var tmpTo = {};
+			tmpTo[address] = amount
+			this.to.push(tmpTo)
+		}
+	}
+	parseOptions(options){
+		if (!options)
+			return
+
+		// Grab the From Addresses, it can be an array or regular.
+		if (options.from){
+			if (Array.isArray(options.from)){
+				for (var addr of options.from){
+					this.addFrom(addr)
 				}
-			} else if (isValidAddress(options.from, this.coin.network)) {
-				this.from.push(new Address(options.from, this.coin));
+			} else {
+				this.addFrom(options.from)
+			}
+		}
+
+		// Load who we are sending to
+		if (options.to){
+			// Check if we are providing an address string and amount seperately
+			if (Array.isArray(options.to)){
+				for (var payTo of options.to){
+					for (var address in payTo){
+						this.addTo(address, payTo[address])
+					}
+				}
+			} else {
+				for (var address in options.to){
+					this.addTo(address, options.to[address])
+				}
 			}
 		}
 	}
---
+++ test/TransactionBuilder.test.js
@@ -0,0 +1,46 @@
+var bip32 = require('bip32');
+var TransactionBuilder = require('../lib/TransactionBuilder');
+var networks = require('../lib/networks');
+var Address = require('../lib/Address');
+
+test("TransactionBuilder should load a From Address", () => {
+	var node = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", networks.flo.network)
+	var address = new Address(node, networks.flo, false);
+
+	var builder = new TransactionBuilder(networks.flo, {
+		from: address
+	})
+
+	expect(builder.from).toContainEqual(address)
+})
+
+test("TransactionBuilder should load a From Address from an Array", () => {
+	var node = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", networks.flo.network)
+	var address = new Address(node, networks.flo, false);
+
+	var builder = new TransactionBuilder(networks.flo, {
+		from: [
+			address
+		]
+	})
+
+	expect(builder.from).toContainEqual(address)
+})
+
+test("TransactionBuilder should load a To Address", () => {
+	var builder = new TransactionBuilder(networks.flo, {
+		to: {"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.001}
+	})
+
+	expect(builder.to).toContainEqual({"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.001})
+})
+
+test("TransactionBuilder should load a To Address from an Array", () => {
+	var builder = new TransactionBuilder(networks.flo, {
+		to: [
+			{"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.001}
+		]
+	})
+
+	expect(builder.to).toContainEqual({"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.001})
+})
