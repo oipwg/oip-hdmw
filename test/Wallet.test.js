036100c29ce120724059c4e0f84a46bfb5bd24d4
Sky Young <sky@alexandria.io>
2018-06-12 15:26:25 -0700

    Add tests for getEntropy on Wallet

--- test/Wallet.test.js
+++ test/Wallet.test.js
@@ -5,12 +5,15 @@ test('Wallet can be defined from Mnemonic', () => {
 
 	expect(w.getSeed()).toBe('5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4')
 	expect(w.getMnemonic()).toBe("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
+	expect(w.getEntropy()).toBe('00000000000000000000000000000000')
 })
 
 test('Wallet can be defined from Seed Hex', () => {
 	var w = new Wallet('5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4', {discover: false})
 
 	expect(w.getSeed()).toBe('5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4')
+	expect(w.getEntropy()).toBeUndefined()
+	expect(w.getMnemonic()).toBeUndefined()
 })
 
 test('Wallet can be defined from BIP39 Entropy', () => {
@@ -18,6 +21,7 @@ test('Wallet can be defined from BIP39 Entropy', () => {
 
 	expect(w.getMnemonic()).toBe("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
 	expect(w.getSeed()).toBe('5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4')
+	expect(w.getEntropy()).toBe('00000000000000000000000000000000')
 })
 
 test('Wallet can spawn on only one defined coin', () => {
