f19dff572b67f0f62f4e9205c923d8a0a3c34b6a
Sky Young <sky@alexandria.io>
2018-06-14 16:02:33 -0700

    Add Documentation to TransactionBuilder

--- src/TransactionBuilder.js
+++ src/TransactionBuilder.js
@@ -6,10 +6,81 @@ import coinselect from 'coinselect'
 import Address from './Address'
 import { isValidPublicAddress } from './util'
 
-const GAP_LIMIT = 20;
+/**
+ * An Output for a Transaction
+ * @typedef {Object} OutputAddress
+ * @example
+ * { "FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.00001 }
+ * @example
+ * { "base58-public-address": valueInWholeCoin }
+ */
 
-module.exports =
+/**
+ * An object returned from `coinselect` that contains information about selected inputs, outputs, and the fee.
+ * @typedef {Object} SelectedInputOutput
+ * @property {Array<TXInput>} inputs - An Array of Transaction Inputs
+ * @property {Array<TXOutput>} outputs - An Array of Transaction Outputs
+ * @property {number} fee - The Calculated Fee to pay
+ */
+
+/**
+ * A Transaction Input
+ * @typedef {Object} TXInput
+ * @property {string} address - Base58 Public Address
+ * @property {string} txId - Parent Transaction ID
+ * @property {number} vout - Index of output in Parent Transaction
+ * @property {string} scriptPubKey - Script Public Key Hash
+ * @property {number} value - Balance of the input in Satoshis 
+ * @example
+ * { 
+ * 	address: 'F8P6nUvDfcHikqdUnoQaGPBVxoMcUSpGDp',
+ * 	txId: '7687e361f00998f96b29938bf5b7d9003a15ec182c13b6ddbd5adc0f993cbf9c',
+ * 	vout: 1,
+ * 	scriptPubKey: '76a9141bfcff1731caf3a16225d3e78735ddc229e4fc6c88ac',
+ * 	value: 100000 
+ * }
+ */
+
+/**
+ * A Transaction Output
+ * @typedef {Object} TXOutput
+ * @property {string} address - Base58 Public Address
+ * @property {number} value - Amount to send Satoshis 
+ * @example
+ * {
+ * 	address: 'FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu',
+ * 	value: 1000
+ * }
+ */
+
+/**
+ * Build & Send Transactions out to the network Easily using Addresses!
+ */
 class TransactionBuilder {
+	/**
+	 * Create a new TransactionBuilder
+	 * ##### Example
+	 * ```
+	 * import bip32 from 'bip32'
+	 * import { Address, TransactionBuilder, Networks } from 'oip-hdmw'
+	 * 
+	 * var node = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", Networks.flo.network)
+	 * var address = new Address(node, Networks.flo, false)
+	 * 
+	 * var builder = new TransactionBuilder(Networks.flo, {
+	 * 	from: address,
+	 * 	to: {"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.00001},
+	 * 	floData: "Testing oip-hdmw!"
+	 * })
+	 * ```
+	 * @param  {CoinInfo} coin - CoinInfo for this specific Network you want to send the Transaction on.
+	 * @param  {Object} [options]
+	 * @param  {Address|Array.<Address>} options.from - The Address(es) to send from.
+	 * @param  {OutputAddress|Array.<OutputAddress>} options.to - The amounts & Address(es) to send to.
+	 * @param  {string} [options.floData=""] - The FloData to be added to the Transaction
+	 * @param  {Account} [account] - An Account to get a Change Address from if needed, if undefined, change will be sent to first `from` Address.
+	 * @return {TransactionBuilder}
+	 */
 	constructor(coin, options, account){
 		this.coin = coin;
 		this.account = account;
@@ -23,15 +94,39 @@ class TransactionBuilder {
 
 		this.parseOptions(options);	
 	}
-	addFrom(addr){
-		if (addr instanceof Address){
-			if (isValidPublicAddress(addr.getPublicAddress(), this.coin.network)){
-				this.from.push(addr)
+	/**
+	 * Add an Address to send from
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Address, TransactionBuilder, Networks } from 'oip-hdmw'
+	 * 
+	 * var node = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", Networks.flo.network)
+	 * var address = new Address(node, Networks.flo, false)
+	 *
+	 * var builder = new TransactionBuilder(Networks.flo)
+	 * builder.addFrom(address);
+	 * @param {Address} address - Address to add to the From Addresses
+	 */
+	addFrom(address){
+		if (address instanceof Address){
+			if (isValidPublicAddress(address.getPublicAddress(), this.coin.network)){
+				this.from.push(address)
 			}
 		} else {
 			throw new Error("From Address MUST BE InstanceOf Address")
 		}
 	}
+	/**
+	 * Add an Address and Amount to send to
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { TransactionBuilder, Networks } from 'oip-hdmw'
+	 *
+	 * var builder = new TransactionBuilder(Networks.flo)
+	 * builder.addTo("FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu", 0.001);
+	 * @param {string} address - Base58 Public Address to send To
+	 * @param {number} amount - Amount to Send (in whole coin)
+	 */
 	addTo(address, amount){
 		if (isValidPublicAddress(address, this.coin.network) && !isNaN(amount)){
 			var tmpTo = {
@@ -41,6 +136,27 @@ class TransactionBuilder {
 			this.to.push(tmpTo)
 		}
 	}
+	/**
+	 * Load From & To addresses
+	 * @param  {Object} options
+	 * @param  {Address|Array.<Address>} options.from - The Address(es) to send from.
+	 * @param  {OutputAddress|Array.<OutputAddress>} options.to - The amounts & Address(es) to send to.
+	 * @param  {string} [options.floData=""] - The FloData to be added to the Transaction
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Address, TransactionBuilder, Networks } from 'oip-hdmw'
+	 * 
+	 * var node = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", Networks.flo.network)
+	 * var address = new Address(node, Networks.flo, false)
+	 * 
+	 * var builder = new TransactionBuilder(Networks.flo)
+	 * 
+	 * builder.parseOptions({
+	 * 	from: address,
+	 * 	to: {"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.00001},
+	 * 	floData: "Testing oip-hdmw!"
+	 * })
+	 */
 	parseOptions(options){
 		if (!options)
 			return
@@ -74,24 +190,69 @@ class TransactionBuilder {
 
 		this.passedOptions = options;
 	}
+	/**
+	 * Get the Unspent Transaction Outputs for all the From addresses specified.
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Address, TransactionBuilder, Networks } from 'oip-hdmw'
+	 * 
+	 * var node = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", Networks.flo.network)
+	 * var address = new Address(node, Networks.flo, false)
+	 * 
+	 * var builder = new TransactionBuilder(Networks.flo, {
+	 * 	from: address,
+	 * 	to: {"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.00001}
+	 * })
+	 *
+	 * builder.getUnspents().then((utxos) => {
+	 * 	console.log(utxos)
+	 * })
+	 * @return {Promise<Array.<utxo>>} Returns a Promise that will resolve to an Array of unspent utxos
+	 */
 	getUnspents(){
 		var addresses = this.from.map((address) => { return address.getPublicAddress() });
 
 		return this.coin.explorer.getAddressesUtxo(addresses).then((utxos) => {
-			return utxos.map((utxo) => {
-				return {
-					address: utxo.address,
-					txId: utxo.txid,
-					vout: utxo.vout,
-					scriptPubKey: utxo.scriptPubKey,
-					value: utxo.satoshis
-				}
-			})
+			return utxos
 		})
 	}
+	/**
+	 * Get calculated Inputs and Outputs (and Fee) for From and To Addresses
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Account, Address, TransactionBuilder, Networks } from 'oip-hdmw'
+	 *
+	 * var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", networks.flo.network)
+	 * var account = new Account(accountMaster, networks.flo, false);
+	 * 
+	 * var node = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", Networks.flo.network)
+	 * var address = new Address(node, Networks.flo, false)
+	 * 
+	 * var builder = new TransactionBuilder(Networks.flo, {
+	 * 	from: address,
+	 * 	to: {"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.00001}
+	 * }, account)
+	 *
+	 * builder.buildInputsAndOutputs().then((calculated) => {
+	 * 	console.log(calculated.inputs)
+	 * 	console.log(calculated.outputs)
+	 * 	console.log(calculated.fee)
+	 * })
+	 * @return {SelectedInputOutput} 
+	 */
 	buildInputsAndOutputs(){
 		return this.discoverChange().then(() => { 
 			return this.getUnspents().then((utxos) => {
+				var formattedUtxos = utxos.map((utxo) => {
+					return {
+						address: utxo.address,
+						txId: utxo.txid,
+						vout: utxo.vout,
+						scriptPubKey: utxo.scriptPubKey,
+						value: utxo.satoshis
+					}
+				})
+
 				var targets = this.to.map((toObj) => {
 					return {
 						address: toObj.address,
@@ -105,10 +266,32 @@ class TransactionBuilder {
 				if (extraBytes)
 					extraBytesLength = extraBytes.length
 
-				return coinselect(utxos, targets, Math.ceil(this.coin.feePerByte), extraBytesLength)
+				return coinselect(formattedUtxos, targets, Math.ceil(this.coin.feePerByte), extraBytesLength)
 			})
 		})
 	}
+	/**
+	 * Discover the used change addresses if we were passed an Account to discover from.
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Account, Address, TransactionBuilder, Networks } from 'oip-hdmw'
+	 *
+	 * var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", networks.flo.network)
+	 * var account = new Account(accountMaster, networks.flo, false);
+	 * 
+	 * var node = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", Networks.flo.network)
+	 * var address = new Address(node, Networks.flo, false)
+	 * 
+	 * var builder = new TransactionBuilder(Networks.flo, {
+	 * 	from: address,
+	 * 	to: {"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.00001}
+	 * }, account)
+	 * 
+	 * builder.discoverChange().then(() => {
+	 * 	console.log("Done Discovering Change!")
+	 * })
+	 * @return {Promise}
+	 */
 	discoverChange(){
 		return new Promise((resolve, reject) => {
 			if (this.account){
@@ -120,6 +303,30 @@ class TransactionBuilder {
 			}
 		})
 	}
+	/**
+	 * Build the Transaction hex for the From and To addresses
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Address, TransactionBuilder, Networks } from 'oip-hdmw'
+	 * 
+	 * var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", networks.flo.network)
+	 * var account = new Account(accountMaster, networks.flo, false);
+	 * 
+	 * // F8P6nUvDfcHikqdUnoQaGPBVxoMcUSpGDp
+	 * var addressNode = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", networks.flo.network)
+	 * var address = new Address(addressNode, networks.flo, false);
+	 * 
+	 * var builder = new TransactionBuilder(networks.flo, {
+	 * 	from: address,
+	 * 	to: {"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.00001},
+	 * 	floData: "Testing oip-hdmw!"
+	 * }, account)
+	 * 
+	 * builder.buildTX().then((hex) => {
+	 * 	console.log(hex)
+	 * })
+	 * @return {Promise<string>} Returns a Promise that resolves to the calculated Transaction Hex
+	 */
 	buildTX(){
 		return this.buildInputsAndOutputs().then((selected) => {
 			var inputs = selected.inputs;
@@ -169,6 +376,30 @@ class TransactionBuilder {
 			return builtHex
 		})
 	}
+	/**
+	 * Build & Send the Transaction that we have been forming
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Address, TransactionBuilder, Networks } from 'oip-hdmw'
+	 * 
+	 * var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", networks.flo.network)
+	 * var account = new Account(accountMaster, networks.flo, false);
+	 * 
+	 * // F8P6nUvDfcHikqdUnoQaGPBVxoMcUSpGDp
+	 * var addressNode = bip32.fromBase58("Fprv52CvMcVNkt3jU7MjybjTNie1Bqm7T66KBueSVFW74hXH43sXMAUdmk73TENACSHhHbwm7ZnHiaW3DxtkwhsbtpNjsh4EpnFVjZVJS7oxNqw", networks.flo.network)
+	 * var address = new Address(addressNode, networks.flo, false);
+	 * 
+	 * var builder = new TransactionBuilder(networks.flo, {
+	 * 	from: address,
+	 * 	to: {"FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu": 0.00001},
+	 * 	floData: "Testing oip-hdmw!"
+	 * }, account)
+	 * 
+	 * builder.sendTX().then((txid) => {
+	 * 	console.log(txid)
+	 * })
+	 * @return {Promise<string>} Returns a promise that will resolve to the success TXID
+	 */
 	sendTX(){
 		return new Promise((resolve, reject) => {
 			this.buildTX().then((hex) => {
@@ -179,3 +410,5 @@ class TransactionBuilder {
 		})
 	}
 }
+
+module.exports = TransactionBuilder
