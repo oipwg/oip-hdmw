f6b46a4376d1e770a069263311841ebed0766454
Sky Young <sky@alexandria.io>
2018-06-14 13:54:46 -0700

    Add Documentation for Account

--- src/Account.js
+++ src/Account.js
@@ -16,15 +16,66 @@ const GAP_LIMIT = 20;
 const CUSTOM_ADDRESS_FUNCTION = (node, network) => {
 	return { address: node, network: network }
 }
+/**
+ * A BIP32 Node that manages Derivation of Chains and Addresses. This is created from the [`bip32` npm package managed by `bitcoinjs`](https://github.com/bitcoinjs/bip32).
+ * @typedef {Object} bip32
+ * @example <caption>Spawn a Bitcoin bip32 Node</caption>
+ * import bip32 from 'bip32';
+ * 
+ * var bip32Node = bip32.fromBase58("xprv9xpXFhFpqdQK3TmytPBqXtGSwS3DLjojFhTGht8gwAAii8py5X6pxeBnQ6ehJiyJ6nDjWGJfZ95WxByFXVkDxHXrqu53WCRGypk2ttuqncb")
+ * @example <caption>Spawn a Flo bip32 Node</caption>
+ * import bip32 from 'bip32';
+ * import { Networks } from 'oip-hdmw';
+ * 
+ * var bip32Node = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+ */
 
-module.exports =
+/**
+ * A BIP32 Chain manager. This is created from the [`bip32-utils` npm package managed by `bitcoinjs`](https://github.com/bitcoinjs/bip32-utils).
+ * @typedef {Object} bip32utilschain
+ * @example
+ * import bip32 from 'bip32';
+ * import bip32utils from 'bip32-utils';
+ * 
+ * var bip32Node = bip32.fromBase58("xprv9xpXFhFpqdQK3TmytPBqXtGSwS3DLjojFhTGht8gwAAii8py5X6pxeBnQ6ehJiyJ6nDjWGJfZ95WxByFXVkDxHXrqu53WCRGypk2ttuqncb")
+ * var chain = new bip32utils.Chain(bip32Node)
+ */
+
+/**
+ * Manages Chains and Addresses for a specific BIP32/BIP44 Account
+ */
 class Account {
-	constructor(accountMaster, coin, discover){
-		this.accountMaster = accountMaster;
+	/**
+	 * Create a new Account to manage Chains and Addresses for based on a BIP32 Node
+	 *
+	 * ##### Examples
+	 * Create a Bitcoin Account
+	 * ```
+	 * import { Account, Networks } from 'oip-hdmw';
+	 *
+	 * var account_master = bip32.fromBase58("xprv9xpXFhFpqdQK3TmytPBqXtGSwS3DLjojFhTGht8gwAAii8py5X6pxeBnQ6ehJiyJ6nDjWGJfZ95WxByFXVkDxHXrqu53WCRGypk2ttuqncb")
+	 *
+	 * var account = new Account(account_master, Networks.bitcoin);
+	 * ```
+	 * Create a Flo Account
+	 * ```
+	 * import { Account, Networks } from 'oip-hdmw';
+	 *
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo);
+	 * ```
+	 * @param  {bip32} account_master - The BIP32 Node to derive Chains and Addresses from.
+	 * @param  {CoinInfo} coin - The CoinInfo for the Account
+	 * @param  {boolean} [discover=true] - Should the Account auto-discover Chains and Addresses
+	 * @return {Account}
+	 */
+	constructor(account_master, coin, discover){
+		this.account_master = account_master;
 		this.coin = coin || {};
 
-		var external = this.accountMaster.derive(0)
-		var internal = this.accountMaster.derive(1)
+		var external = this.account_master.derive(0)
+		var internal = this.account_master.derive(1)
 
 		this.account = new bip32utils.Account([
 			new bip32utils.Chain(external, undefined, CUSTOM_ADDRESS_FUNCTION),
@@ -50,11 +101,58 @@ class Account {
 			this.discoverChain(1)
 		}
 	}
+	/**
+	 * Get the Main Address for a specified Chain and Index on the Chain.
+	 * @param  {number}	[chain_number=0] - Number of the specific chain you want to get the Main Address for
+	 * @param  {number} [main_address_number=0] - Index of the Main Address on the specified chain
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * var address = account.getMainAddress()
+	 * // address.getPublicAddress() = FPznv9i9iHX5vt4VMbH9x2LgUcrjtSn4cW
+	 * @return {Address}
+	 */
 	getMainAddress(chain_number, main_address_number){
-		var addr = CUSTOM_ADDRESS_FUNCTION(this.account.getChain(chain_number || 0).__parent.derive(main_address_number || 0), this.coin.network);
+		return this.getAddress(chain_number, main_address_number)
+	}
+	/**
+	 * Get the Address for a specified Chain and Index on the Chain.
+	 * @param  {number}	[chain_number=0] - Number of the specific chain you want to get the Address from
+	 * @param  {number} [address_number=0] - Index of the Address on the specified chain
+	 * @example <caption>Get the address on Chain `0` at Index `10`</caption>
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * var address = account.getAddress(0, 10)
+	 * // address.getPublicAddress() = F8P6nUvDfcHikqdUnoQaGPBVxoMcUSpGDp
+	 * @return {Address}
+	 */
+	getAddress(chain_number, address_number){
+		var addr = CUSTOM_ADDRESS_FUNCTION(this.account.getChain(chain_number || 0).__parent.derive(address_number || 0), this.coin.network);
 		
 		return new Address(addr, this.coin, false)
 	}
+	/**
+	 * Get the Balance for the entire Account
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * account.getBalance().then((balance) => {
+	 * 	console.log(balance);
+	 * })
+	 * @return {Promise<number>} - Returns a Promise that will resolve to the total balance.
+	 */
 	getBalance(){
 		return new Promise((resolve, reject) => {
 			return this.discoverChainsIfNeeded().then(() => {
@@ -68,23 +166,91 @@ class Account {
 			})
 		})
 	}
-	getNextChainAddress(chain){
-		// We use Chain 1 since that is the "Internal" chain used for generating change addresses.
-		return new Address(this.account.getChain(chain || 0).next(), this.coin, false);
+	/**
+	 * Get the Next Chain Address for a specified chain
+	 * @param  {number} [chain_number=0] - The specific chain that you want to get the next address from
+	 * @example <caption>Get the next Chain Address on Chain #1</caption>
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * var address = account.getNextChainAddress(1)
+	 * @return {Address}
+	 */
+	getNextChainAddress(chain_number){
+		return new Address(this.account.getChain(chain_number || 0).next(), this.coin, false);
 	}
+	/**
+	 * Get the Next Change Address from the "Internal" chain
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * var address = account.getNextChangeAddress()
+	 * @return {Address}
+	 */
 	getNextChangeAddress(){
 		// We use Chain 1 since that is the "Internal" chain used for generating change addresses.
 		return this.getNextChainAddress(1)
 	}
-	sendTransaction(options){
+	/**
+	 * Send a Payment to specified Addresses and Amounts, NOT YET IMPLEMENTED
+	 * @param  {Object} options - the options for the specific transaction being sent
+	 * @return {Promise<string>} - Returns a promise that will resolve to the success TXID
+	 */
+	sendPayment(options){
 		
 	}
+	/**
+	 * Get the Extended Private Key for the Account
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * var extPrivateKey = account.getExtendedPrivateKey()
+	 * // extPrivateKey = Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC
+	 * @return {string}
+	 */
 	getExtendedPrivateKey(){
-		return this.accountMaster.toBase58()
+		return this.account_master.toBase58()
 	}
+	/**
+	 * Get the Extended Public Key for the Account
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * var extPublicKey = account.getExtendedPublicKey()
+	 * // extPublicKey = Fpub1BPo8vEQqDkoDQmDqcJ8WFHD331AMpd7VU7atCJsix8xbHwN6K9wfDLjZKnW9fUw5uJg8UJMLhQ5W7gTxv6DbkfPoeJbBpMaUHrULxzVnSy
+	 * @return {string}
+	 */
 	getExtendedPublicKey(){
-		return this.accountMaster.neutered().toBase58()
+		return this.account_master.neutered().toBase58()
 	}
+	/**
+	 * Get the specified Chain number
+	 * @param {number} chain_number - The number of the chain you are requesting
+	 * @example <caption>Get Chain 0</caption>
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * var chain = account.getChain(0)
+	 * @return {bip32utilschain}
+	 */
 	getChain(chainNumber){
 		return this.account.getChain(chainNumber)
 	}
@@ -105,9 +271,24 @@ class Account {
 			callback(err, used, checked)
 		})
 	}
-	discoverChain(chainNumber){
+	/**
+	 * Discover Used and Unused addresses for a specified Chain number
+	 * @param  {number} chain_number - The number of the chain you wish to discover
+	 * @example <caption>Discover Chain 0</caption>
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * account.discoverChain(0).then((acc) => {
+	 * 	console.log(acc.getChain(0).addresses)
+	 * })
+	 * @return {Promise<Account>} - A Promise that once finished will resolve to the Account (now with discovery done)
+	 */
+	discoverChain(chain_number){
 		return new Promise((resolve, reject) => {
-			this._discoverChain(chainNumber, GAP_LIMIT, (addresses, callback) => {
+			this._discoverChain(chain_number, GAP_LIMIT, (addresses, callback) => {
 				var results = {};
 
 				var checkComplete = () => {
@@ -142,12 +323,27 @@ class Account {
 				if (err) 
 					reject(err)
 
-				this.discovery[chainNumber] = { lastUpdate: Date.now() }
+				this.discovery[chain_number] = { lastUpdate: Date.now() }
 
-				resolve(this, chainNumber)
+				resolve(this, chain_number)
 			})
 		})
 	}
+	/**
+	 * Discover all Chains if we haven't in the past 30 minutes
+	 * @example
+	 * import bip32 from 'bip32'
+	 * import { Account, Networks } from 'oip-hdmw'
+	 * 
+	 * var account_master = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	 *
+	 * var account = new Account(account_master, Networks.flo, false);
+	 * account.discoverChainsIfNeeded().then((acc) => {
+	 * 	console.log(acc.getChain(0).addresses)
+	 * 	console.log(acc.getChain(1).addresses)
+	 * })
+	 * @return {Promise<Account>} - A Promise that once finished will resolve to the Account (now with discovery done)
+	 */
 	discoverChainsIfNeeded(){
 		return new Promise((resolve, reject) => {
 			var chainsToDiscover = []
@@ -172,3 +368,5 @@ class Account {
 		})
 	}
 }
+
+module.exports = Account
