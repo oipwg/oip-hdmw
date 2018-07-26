6318330924818657799d2b3aea2cd3600d68b4fb
Sky Young <sky@alexandria.io>
2018-07-26 12:57:21 -0700

    Update bitcoinjs-lib to v4.0.1
    This is cloning bitcoinjs-lib from the oipwg `export-classify` branch until `bitcoinjs-lib` exports the `classify` class by default. See this issue here for more informaiton: https://github.com/bitcoinjs/bitcoinjs-lib/issues/1148

--- src/TransactionBuilderHelpers.js
+++ src/TransactionBuilderHelpers.js
@@ -1,223 +1,259 @@
 import bitcoin from 'bitcoinjs-lib';
 
 let Transaction = bitcoin.Transaction;
-let ECSignature = bitcoin.ECSignature;
 let bcrypto = bitcoin.crypto;
 let bscript = bitcoin.script;
-let btemplates = bitcoin.script;
-let scriptTypes = btemplates.types;
+let payments = bitcoin.payments;
+let classify = bitcoin.classify;
+let SCRIPT_TYPES = classify.types;
 
 
 var EMPTY_SCRIPT = Buffer.allocUnsafe(0)
 var ONE = Buffer.from('0000000000000000000000000000000000000000000000000000000000000001', 'hex')
 
 function canSign (input) {
-  return input.prevOutScript !== undefined &&
-    input.signScript !== undefined &&
-    input.pubKeys !== undefined &&
+  return input.signScript !== undefined &&
+    input.signType !== undefined &&
+    input.pubkeys !== undefined &&
     input.signatures !== undefined &&
-    input.signatures.length === input.pubKeys.length &&
-    input.pubKeys.length > 0 &&
+    input.signatures.length === input.pubkeys.length &&
+    input.pubkeys.length > 0 &&
     (
-      input.witness === false ||
-      (input.witness === true && input.value !== undefined)
+      input.hasWitness === false ||
+      input.value !== undefined
     )
 }
 
-function expandOutput (script, scriptType, ourPubKey) {
-  var scriptChunks = bscript.decompile(script) || []
-  if (!scriptType) {
-    scriptType = btemplates.classifyOutput(script)
+function expandOutput (script, ourPubKey) {
+  const type = classify.output(script)
+
+  switch (type) {
+    case SCRIPT_TYPES.P2PKH: {
+      if (!ourPubKey) return { type }
+
+      // does our hash160(pubKey) match the output scripts?
+      const pkh1 = payments.p2pkh({ output: script }).hash
+      const pkh2 = bcrypto.hash160(ourPubKey)
+      if (!pkh1.equals(pkh2)) return { type }
+
+      return {
+        type,
+        pubkeys: [ourPubKey],
+        signatures: [undefined]
+      }
+    }
+
+    case SCRIPT_TYPES.P2WPKH: {
+      if (!ourPubKey) return { type }
+
+      // does our hash160(pubKey) match the output scripts?
+      const wpkh1 = payments.p2wpkh({ output: script }).hash
+      const wpkh2 = bcrypto.hash160(ourPubKey)
+      if (!wpkh1.equals(wpkh2)) return { type }
+
+      return {
+        type,
+        pubkeys: [ourPubKey],
+        signatures: [undefined]
+      }
+    }
+
+    case SCRIPT_TYPES.P2PK: {
+      const p2pk = payments.p2pk({ output: script })
+      return {
+        type,
+        pubkeys: [p2pk.pubkey],
+        signatures: [undefined]
+      }
+    }
+
+    case SCRIPT_TYPES.MULTISIG: {
+      const p2ms = payments.p2ms({ output: script })
+      return {
+        type,
+        pubkeys: p2ms.pubkeys,
+        signatures: p2ms.pubkeys.map(() => undefined)
+      }
+    }
   }
 
-  var pubKeys = []
-
-  switch (scriptType) {
-    // does our hash160(pubKey) match the output scripts?
-    case scriptTypes.P2PKH:
-      if (!ourPubKey) break
-
-      var pkh1 = scriptChunks[2]
-      var pkh2 = bcrypto.hash160(ourPubKey)
-      if (pkh1.equals(pkh2)) pubKeys = [ourPubKey]
-      break
-
-    // does our hash160(pubKey) match the output scripts?
-    case scriptTypes.P2WPKH:
-      if (!ourPubKey) break
-
-      var wpkh1 = scriptChunks[1]
-      var wpkh2 = bcrypto.hash160(ourPubKey)
-      if (wpkh1.equals(wpkh2)) pubKeys = [ourPubKey]
-      break
-
-    case scriptTypes.P2PK:
-      pubKeys = scriptChunks.slice(0, 1)
-      break
-
-    case scriptTypes.MULTISIG:
-      pubKeys = scriptChunks.slice(1, -2)
-      break
-
-    default: return { scriptType: scriptType }
-  }
-
-  return {
-    pubKeys: pubKeys,
-    scriptType: scriptType,
-    signatures: pubKeys.map(function () { return undefined })
-  }
+  return { type }
 }
 
-function checkP2SHInput (input, redeemScriptHash) {
-  if (input.prevOutType) {
-    if (input.prevOutType !== scriptTypes.P2SH) throw new Error('PrevOutScript must be P2SH')
-
-    var chunks = bscript.decompile(input.prevOutScript)
-    if (!chunks) throw new Error('Invalid prevOutScript')
-    if (!chunks[1].equals(redeemScriptHash)) throw new Error('Inconsistent hash160(redeemScript)')
-  }
-}
-
-function checkP2WSHInput (input, witnessScriptHash) {
-  if (input.prevOutType) {
-    if (input.prevOutType !== scriptTypes.P2WSH) throw new Error('PrevOutScript must be P2WSH')
-
-    var chunks = bscript.decompile(input.prevOutScript)
-    if (!chunks) throw new Error('Invalid witnessScript')
-    if (!chunks[1].equals(witnessScriptHash)) throw new Error('Inconsistent sha256(witnessScript)')
-  }
-}
-
-function prepareInput (input, kpPubKey, redeemScript, witnessValue, witnessScript) {
-  var expanded
-  var prevOutType
-  var prevOutScript
-
-  var p2sh = false
-  var p2shType
-  var redeemScriptHash
-
-  var witness = false
-  var p2wsh = false
-  var witnessType
-  var witnessScriptHash
-
-  var signType
-  var signScript
-
+function prepareInput (input, ourPubKey, redeemScript, witnessValue, witnessScript) {
   if (redeemScript && witnessScript) {
-    redeemScriptHash = bcrypto.hash160(redeemScript)
-    witnessScriptHash = bcrypto.sha256(witnessScript)
-    checkP2SHInput(input, redeemScriptHash)
+    const p2wsh = payments.p2wsh({ redeem: { output: witnessScript } })
+    const p2wshAlt = payments.p2wsh({ output: redeemScript })
+    const p2sh = payments.p2sh({ redeem: { output: redeemScript } })
+    const p2shAlt = payments.p2sh({ redeem: p2wsh })
 
-    if (!redeemScript.equals(btemplates.witnessScriptHash.output.encode(witnessScriptHash))) throw new Error('Witness script inconsistent with redeem script')
+    // enforces P2SH(P2WSH(...))
+    if (!p2wsh.hash.equals(p2wshAlt.hash)) throw new Error('Witness script inconsistent with prevOutScript')
+    if (!p2sh.hash.equals(p2shAlt.hash)) throw new Error('Redeem script inconsistent with prevOutScript')
 
-    expanded = expandOutput(witnessScript, undefined, kpPubKey)
-    if (!expanded.pubKeys) throw new Error(expanded.scriptType + ' not supported as witnessScript (' + bscript.toASM(witnessScript) + ')')
-
-    prevOutType = btemplates.types.P2SH
-    prevOutScript = btemplates.scriptHash.output.encode(redeemScriptHash)
-    p2sh = witness = p2wsh = true
-    p2shType = btemplates.types.P2WSH
-    signType = witnessType = expanded.scriptType
-    signScript = witnessScript
-  } else if (redeemScript) {
-    redeemScriptHash = bcrypto.hash160(redeemScript)
-    checkP2SHInput(input, redeemScriptHash)
-
-    expanded = expandOutput(redeemScript, undefined, kpPubKey)
-    if (!expanded.pubKeys) throw new Error(expanded.scriptType + ' not supported as redeemScript (' + bscript.toASM(redeemScript) + ')')
-
-    prevOutType = btemplates.types.P2SH
-    prevOutScript = btemplates.scriptHash.output.encode(redeemScriptHash)
-    p2sh = true
-    signType = p2shType = expanded.scriptType
-    signScript = redeemScript
-    witness = signType === btemplates.types.P2WPKH
-  } else if (witnessScript) {
-    witnessScriptHash = bcrypto.sha256(witnessScript)
-    checkP2WSHInput(input, witnessScriptHash)
-
-    expanded = expandOutput(witnessScript, undefined, kpPubKey)
-    if (!expanded.pubKeys) throw new Error(expanded.scriptType + ' not supported as witnessScript (' + bscript.toASM(witnessScript) + ')')
-
-    prevOutType = btemplates.types.P2WSH
-    prevOutScript = btemplates.witnessScriptHash.output.encode(witnessScriptHash)
-    witness = p2wsh = true
-    signType = witnessType = expanded.scriptType
-    signScript = witnessScript
-  } else if (input.prevOutType) {
-    // embedded scripts are not possible without a redeemScript
-    if (input.prevOutType === scriptTypes.P2SH) {
-      throw new Error('PrevOutScript is ' + input.prevOutType + ', requires redeemScript')
+    const expanded = expandOutput(p2wsh.redeem.output, ourPubKey)
+    if (!expanded.pubkeys) throw new Error(expanded.type + ' not supported as witnessScript (' + bscript.toASM(witnessScript) + ')')
+    if (input.signatures && input.signatures.some(x => x)) {
+      expanded.signatures = input.signatures
     }
 
-    if (input.prevOutType === scriptTypes.P2WSH) {
-      throw new Error('PrevOutScript is ' + input.prevOutType + ', requires witnessScript')
+    let signScript = witnessScript
+    if (expanded.type === SCRIPT_TYPES.P2WPKH) throw new Error('P2SH(P2WSH(P2WPKH)) is a consensus failure')
+
+    return {
+      redeemScript,
+      redeemScriptType: SCRIPT_TYPES.P2WSH,
+
+      witnessScript,
+      witnessScriptType: expanded.type,
+
+      prevOutType: SCRIPT_TYPES.P2SH,
+      prevOutScript: p2sh.output,
+
+      hasWitness: true,
+      signScript,
+      signType: expanded.type,
+
+      pubkeys: expanded.pubkeys,
+      signatures: expanded.signatures
+    }
+  }
+
+  if (redeemScript) {
+    const p2sh = payments.p2sh({ redeem: { output: redeemScript } })
+
+    if (input.prevOutScript) {
+      let p2shAlt
+      try {
+        p2shAlt = payments.p2sh({ output: input.prevOutScript })
+      } catch (e) { throw new Error('PrevOutScript must be P2SH') }
+      if (!p2sh.hash.equals(p2shAlt.hash)) throw new Error('Redeem script inconsistent with prevOutScript')
     }
 
-    prevOutType = input.prevOutType
-    prevOutScript = input.prevOutScript
-    expanded = expandOutput(input.prevOutScript, input.prevOutType, kpPubKey)
-    if (!expanded.pubKeys) return
+    const expanded = expandOutput(p2sh.redeem.output, ourPubKey)
+    if (!expanded.pubkeys) throw new Error(expanded.type + ' not supported as redeemScript (' + bscript.toASM(redeemScript) + ')')
+    if (input.signatures && input.signatures.some(x => x)) {
+      expanded.signatures = input.signatures
+    }
 
-    witness = (input.prevOutType === scriptTypes.P2WPKH)
-    signType = prevOutType
-    signScript = prevOutScript
-  } else {
-    prevOutScript = btemplates.pubKeyHash.output.encode(bcrypto.hash160(kpPubKey))
-    expanded = expandOutput(prevOutScript, scriptTypes.P2PKH, kpPubKey)
+    let signScript = redeemScript
+    if (expanded.type === SCRIPT_TYPES.P2WPKH) {
+      signScript = payments.p2pkh({ pubkey: expanded.pubkeys[0] }).output
+    }
 
-    prevOutType = scriptTypes.P2PKH
-    witness = false
-    signType = prevOutType
-    signScript = prevOutScript
+    return {
+      redeemScript,
+      redeemScriptType: expanded.type,
+
+      prevOutType: SCRIPT_TYPES.P2SH,
+      prevOutScript: p2sh.output,
+
+      hasWitness: expanded.type === SCRIPT_TYPES.P2WPKH,
+      signScript,
+      signType: expanded.type,
+
+      pubkeys: expanded.pubkeys,
+      signatures: expanded.signatures
+    }
   }
 
-  if (signType === scriptTypes.P2WPKH) {
-    signScript = btemplates.pubKeyHash.output.encode(btemplates.witnessPubKeyHash.output.decode(signScript))
+  if (witnessScript) {
+    const p2wsh = payments.p2wsh({ redeem: { output: witnessScript } })
+
+    if (input.prevOutScript) {
+      const p2wshAlt = payments.p2wsh({ output: input.prevOutScript })
+      if (!p2wsh.hash.equals(p2wshAlt.hash)) throw new Error('Witness script inconsistent with prevOutScript')
+    }
+
+    const expanded = expandOutput(p2wsh.redeem.output, ourPubKey)
+    if (!expanded.pubkeys) throw new Error(expanded.type + ' not supported as witnessScript (' + bscript.toASM(witnessScript) + ')')
+    if (input.signatures && input.signatures.some(x => x)) {
+      expanded.signatures = input.signatures
+    }
+
+    let signScript = witnessScript
+    if (expanded.type === SCRIPT_TYPES.P2WPKH) throw new Error('P2WSH(P2WPKH) is a consensus failure')
+
+    return {
+      witnessScript,
+      witnessScriptType: expanded.type,
+
+      prevOutType: SCRIPT_TYPES.P2WSH,
+      prevOutScript: p2wsh.output,
+
+      hasWitness: true,
+      signScript,
+      signType: expanded.type,
+
+      pubkeys: expanded.pubkeys,
+      signatures: expanded.signatures
+    }
   }
 
-  if (p2sh) {
-    input.redeemScript = redeemScript
-    input.redeemScriptType = p2shType
+  if (input.prevOutType && input.prevOutScript) {
+    // embedded scripts are not possible without extra information
+    if (input.prevOutType === SCRIPT_TYPES.P2SH) throw new Error('PrevOutScript is ' + input.prevOutType + ', requires redeemScript')
+    if (input.prevOutType === SCRIPT_TYPES.P2WSH) throw new Error('PrevOutScript is ' + input.prevOutType + ', requires witnessScript')
+    if (!input.prevOutScript) throw new Error('PrevOutScript is missing')
+
+    const expanded = expandOutput(input.prevOutScript, ourPubKey)
+    if (!expanded.pubkeys) throw new Error(expanded.type + ' not supported (' + bscript.toASM(input.prevOutScript) + ')')
+    if (input.signatures && input.signatures.some(x => x)) {
+      expanded.signatures = input.signatures
+    }
+
+    let signScript = input.prevOutScript
+    if (expanded.type === SCRIPT_TYPES.P2WPKH) {
+      signScript = payments.p2pkh({ pubkey: expanded.pubkeys[0] }).output
+    }
+
+    return {
+      prevOutType: expanded.type,
+      prevOutScript: input.prevOutScript,
+
+      hasWitness: expanded.type === SCRIPT_TYPES.P2WPKH,
+      signScript,
+      signType: expanded.type,
+
+      pubkeys: expanded.pubkeys,
+      signatures: expanded.signatures
+    }
   }
 
-  if (p2wsh) {
-    input.witnessScript = witnessScript
-    input.witnessScriptType = witnessType
-  }
+  const prevOutScript = payments.p2pkh({ pubkey: ourPubKey }).output
+  return {
+    prevOutType: SCRIPT_TYPES.P2PKH,
+    prevOutScript: prevOutScript,
 
-  input.pubKeys = expanded.pubKeys
-  input.signatures = expanded.signatures
-  input.signScript = signScript
-  input.signType = signType
-  input.prevOutScript = prevOutScript
-  input.prevOutType = prevOutType
-  input.witness = witness
+    hasWitness: false,
+    signScript: prevOutScript,
+    signType: SCRIPT_TYPES.P2PKH,
+
+    pubkeys: [ourPubKey],
+    signatures: [undefined]
+  }
 }
 
 function sign(transactionBuilder, extraBytes, vin, keyPair, redeemScript, hashType, witnessValue, witnessScript){
-  if (!transactionBuilder.inputs[vin])
+  if (!transactionBuilder.__inputs[vin])
     throw new Error("No input at index: " + vin)
 
   hashType = hashType || bitcoin.Transaction.SIGHASH_ALL
 
-  var input = transactionBuilder.inputs[vin]
+  var input = transactionBuilder.__inputs[vin]
 
   // if redeemScript was previously provided, enforce consistency
   if (input.redeemScript !== undefined && redeemScript && !input.redeemScript.equals(redeemScript)) {
     throw new Error('Inconsistent redeemScript')
   }
 
-  var kpPubKey = keyPair.publicKey
+  var ourPubKey = keyPair.publicKey
 
   // Check both publickKey, getPublicKey, and getPublicKeyBuffer to support all HDNode types (bitcoinjs-lib v3 & bip32 npm & bitcoinjs-lib v4 when it comes out)
-  if (!kpPubKey && keyPair.getPublicKey)
-    kpPubKey = keyPair.getPublicKey()
-  else if (!kpPubKey && keyPair.getPublicKeyBuffer)
-    kpPubKey = keyPair.getPublicKeyBuffer()
+  if (!ourPubKey && keyPair.getPublicKey)
+    ourPubKey = keyPair.getPublicKey()
+  else if (!ourPubKey && keyPair.getPublicKeyBuffer)
+    ourPubKey = keyPair.getPublicKeyBuffer()
 
   if (!canSign(input)) {
     if (witnessValue !== undefined) {
@@ -228,41 +264,34 @@ function sign(transactionBuilder, extraBytes, vin, keyPair, redeemScript, hashTy
     }
 
     // @TODO: Add this when SegWit support is needed
-    if (!canSign(input)) prepareInput(input, kpPubKey, redeemScript, witnessValue, witnessScript)
+    if (!canSign(input)){
+      const prepared = prepareInput(input, ourPubKey, redeemScript, witnessValue, witnessScript)
+
+      Object.assign(input, prepared)
+    }
+
     if (!canSign(input)) throw Error(input.prevOutType + ' not supported')
   }
 
   var signatureHash
   if (input.witness) {
-    signatureHash = transactionBuilder.tx.hashForWitnessV0(vin, input.signScript, input.value, hashType)
-    // Will need to turn to the following when bitcoinjs-lib v4 comes out
-    // signatureHash = transactionBuilder.__tx.hashForWitnessV0(vin, input.signScript, input.value, hashType)
+    signatureHash = transactionBuilder.__tx.hashForWitnessV0(vin, input.signScript, input.value, hashType)
   } else {
     // signatureHash = transactionBuilder.tx.hashForSignature(vin, input.signScript, hashType)
-    signatureHash = hashForSignature(transactionBuilder.tx, extraBytes, vin, input.signScript, hashType)
-    // Will need to turn to the following when bitcoinjs-lib v4 comes out
-    // signatureHash = hashForSignature(transactionBuilder.__tx, extraBytes, vin, input.signScript, hashType)
+    signatureHash = hashForSignature(transactionBuilder.__tx, extraBytes, vin, input.signScript, hashType)
   }
 
-  var signed = input.pubKeys.some(function (pubKey, i) {
-    if (!kpPubKey.equals(pubKey)) return false
+  var signed = input.pubkeys.some(function (pubKey, i) {
+    if (!ourPubKey.equals(pubKey)) return false
     if (input.signatures[i]) throw new Error('Signature already exists')
 
-    if (kpPubKey.length !== 33 && (
-      input.signType === scriptTypes.P2WPKH ||
-      input.redeemScriptType === scriptTypes.P2WSH ||
-      input.prevOutType === scriptTypes.P2WSH
-    )) throw new Error('BIP143 rejects uncompressed public keys in P2WPKH or P2WSH')
+    if (ourPubKey.length !== 33 && input.hasWitness) {
+      throw new Error('BIP143 rejects uncompressed public keys in P2WPKH or P2WSH')
+    }
 
     let signature = keyPair.sign(signatureHash)
 
-
-    if (Buffer.isBuffer(signature)) 
-      signature = ECSignature.fromRSBuffer(signature)
-
-    input.signatures[i] = signature.toScriptSignature(hashType)
-    // Will need to turn to the following when bitcoinjs-lib v4 comes out
-    // input.signatures[i] = bscript.signature.encode(signature, hashType)
+    input.signatures[i] = bscript.signature.encode(signature, hashType)
     return true
   })
 
@@ -287,9 +316,9 @@ function hashForSignature(transaction, extraBytes, inIndex, prevOutScript, hashT
 
     // ignore sequence numbers (except at inIndex)
     txTmp.ins.forEach(function (input, i) {
-    if (i === inIndex) return
+      if (i === inIndex) return
 
-    input.sequence = 0
+      input.sequence = 0
     })
 
   // SIGHASH_SINGLE: ignore all outputs, except at the same index?
