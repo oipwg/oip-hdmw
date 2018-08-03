86f4379881028df1db331ff08cd78cff77b433ed
Sky Young <sky@alexandria.io>
2018-08-03 10:41:21 -0700

    Retab util file

--- src/util.js
+++ src/util.js
@@ -8,27 +8,27 @@ import varuint from 'varuint-bitcoin'
 /** @module util */
 
 function ripemd160 (buffer) {
-  return createHash('rmd160').update(buffer).digest()
+	return createHash('rmd160').update(buffer).digest()
 }
 
 function sha256 (buffer) {
-  return createHash('sha256').update(buffer).digest()
+	return createHash('sha256').update(buffer).digest()
 }
 
 function hash256 (buffer) {
-  return sha256(sha256(buffer))
+	return sha256(sha256(buffer))
 }
 
 function hash160 (buffer) {
-  return ripemd160(sha256(buffer))
+	return ripemd160(sha256(buffer))
 }
 
 function toBase58Check (hash, version) {
-  var payload = Buffer.allocUnsafe(21)
-  payload.writeUInt8(version, 0)
-  hash.copy(payload, 1)
+	var payload = Buffer.allocUnsafe(21)
+	payload.writeUInt8(version, 0)
+	hash.copy(payload, 1)
 
-  return bs58check.encode(payload)
+	return bs58check.encode(payload)
 }
 
 /**
@@ -37,11 +37,11 @@ function toBase58Check (hash, version) {
  * @return {string} Returns the Base58 encoded Key
  */
 function toBase58 (key, version) {
-  if (!key){
-    return console.log("KEY NULL!!!!")
-  }
+	if (!key){
+		return console.log("KEY NULL!!!!")
+	}
 
-  return toBase58Check(hash160(key), version)
+	return toBase58Check(hash160(key), version)
 }
 
 /**
@@ -51,18 +51,18 @@ function toBase58 (key, version) {
  * @return {Boolean}
  */
 function isValidWIF (key, network) {
-  try {
-    let dec = wif.decode(key);
+	try {
+		let dec = wif.decode(key);
 
-    if (network) {
-      return dec.version === network.wif
-    } else {
-      return true
-    }
-  } catch (e) {
-    console.error(e);
-    return false
-  }
+		if (network) {
+			return dec.version === network.wif
+		} else {
+			return true
+		}
+	} catch (e) {
+		console.error(e);
+		return false
+	}
 }
 
 /**
@@ -72,16 +72,16 @@ function isValidWIF (key, network) {
  * @return {Boolean}
  */
 function isValidPublicAddress (address, network) {
-  try {
-    let dec = bitcoin.address.fromBase58Check(address)
-    if (network) {
-      return dec.version === network.pubKeyHash || dec.version === network.scriptHash
-    } else {
-      return true
-    }
-  } catch (e) {
-    return false
-  }
+	try {
+		let dec = bitcoin.address.fromBase58Check(address)
+		if (network) {
+			return dec.version === network.pubKeyHash || dec.version === network.scriptHash
+		} else {
+			return true
+		}
+	} catch (e) {
+		return false
+	}
 }
 
 // https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#account-discovery
@@ -144,10 +144,10 @@ async function discovery (chain, gapLimit, queryPromise, i, coin) {
  * @return {Boolean}
  */
 function isMnemonic(mnemonic){
-  if (typeof mnemonic === "string" && mnemonic.split(" ").length >= 2)
-    return true
+	if (typeof mnemonic === "string" && mnemonic.split(" ").length >= 2)
+		return true
 
-  return false
+	return false
 }
 
 /**
@@ -156,20 +156,20 @@ function isMnemonic(mnemonic){
  * @return {Boolean}
  */
 function isEntropy(entropy){
-  if (typeof entropy === "string" && entropy.length >= 16 && entropy.length <= 32)
-    return true
+	if (typeof entropy === "string" && entropy.length >= 16 && entropy.length <= 32)
+		return true
 
-  return false
+	return false
 }
 
 module.exports = {
-  hash160,
-  hash256,
-  toBase58,
+	hash160,
+	hash256,
+	toBase58,
 	isValidPublicAddress,
-  isValidWIF,
-  isMnemonic,
-  isEntropy,
-  discovery,
-  varIntBuffer: varuint.encode
+	isValidWIF,
+	isMnemonic,
+	isEntropy,
+	discovery,
+	varIntBuffer: varuint.encode
 }
