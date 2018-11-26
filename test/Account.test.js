55e40ef456c59884eb76400b7ee2c2416925ab8f
orpheus <ryanachacon@gmail.com>
2018-11-26 07:35:54 -0800

    var -> let

--- test/Account.test.js
+++ test/Account.test.js
@@ -1,11 +1,11 @@
-var bip32 = require('bip32');
-var Account = require('../src').Account;
-var Networks = require('../src').Networks;
+let bip32 = require('bip32');
+let Account = require('../src').Account;
+let Networks = require('../src').Networks;
 
 test('Account keys generated from Mnemonic Match', () => {
-	var accountMaster = bip32.fromBase58("xprv9xpXFhFpqdQK3TmytPBqXtGSwS3DLjojFhTGht8gwAAii8py5X6pxeBnQ6ehJiyJ6nDjWGJfZ95WxByFXVkDxHXrqu53WCRGypk2ttuqncb")
+	let accountMaster = bip32.fromBase58("xprv9xpXFhFpqdQK3TmytPBqXtGSwS3DLjojFhTGht8gwAAii8py5X6pxeBnQ6ehJiyJ6nDjWGJfZ95WxByFXVkDxHXrqu53WCRGypk2ttuqncb")
 	
-	var account = new Account(accountMaster, Networks.bitcoin, {discover: false});
+	let account = new Account(accountMaster, Networks.bitcoin, {discover: false});
 
 	expect(account.getExtendedPrivateKey()).toBe("xprv9xpXFhFpqdQK3TmytPBqXtGSwS3DLjojFhTGht8gwAAii8py5X6pxeBnQ6ehJiyJ6nDjWGJfZ95WxByFXVkDxHXrqu53WCRGypk2ttuqncb")
 	expect(account.getExtendedPublicKey()).toBe("xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj")
@@ -118,11 +118,11 @@ test('Account serialize and deserialize', () => {
 })
 
 test('Account auto-discover by default', (done) => {
-	var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	let accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
 	
-	var account = new Account(accountMaster, Networks.flo);
+	let account = new Account(accountMaster, Networks.flo);
 
-	var checkIfComplete = () => {
+	let checkIfComplete = () => {
 		if (account && account.getChain(0) && account.getChain(0).addresses.length >= 25){
 			expect(account.getChain(0).addresses.length).toBeGreaterThan(25)
 			done()
@@ -135,42 +135,42 @@ test('Account auto-discover by default', (done) => {
 }, 20000)
 
 test('Get Account Balance of all Chain Addresses', async (done) => {
-	var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	let accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
 	
-	var account = new Account(accountMaster, Networks.flo, {discover: false});
+	let account = new Account(accountMaster, Networks.flo, {discover: false});
 
-	var balance_obj = await account.getBalance({ discover: true })
+	let balance_obj = await account.getBalance({ discover: true })
 
 	expect(balance_obj.balance).toBeGreaterThan(0)
 	done()
 }, 20000)
 
 test('Account, Get Balance of single address', async (done) => {
-	var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	let accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
 	
-	var account = new Account(accountMaster, Networks.flo, {discover: false});
+	let account = new Account(accountMaster, Networks.flo, {discover: false});
 
-	var balance_obj = await account.getBalance({ discover: true, addresses: "F8P6nUvDfcHikqdUnoQaGPBVxoMcUSpGDp" })
+	let balance_obj = await account.getBalance({ discover: true, addresses: "F8P6nUvDfcHikqdUnoQaGPBVxoMcUSpGDp" })
 	
 	expect(balance_obj.balance).toBeDefined()
 	done()
 }, 20000)
 
 test('Account, Get Balance of multiple addresses', async (done) => {
-	var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	let accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
 	
-	var account = new Account(accountMaster, Networks.flo, {discover: false});
+	let account = new Account(accountMaster, Networks.flo, {discover: false});
 
-	var balance_obj = await account.getBalance({ discover: true, addresses: ["F8P6nUvDfcHikqdUnoQaGPBVxoMcUSpGDp", "FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu"] })
+	let balance_obj = await account.getBalance({ discover: true, addresses: ["F8P6nUvDfcHikqdUnoQaGPBVxoMcUSpGDp", "FHQvhgDut1rn1nvQRZ3z9QgMEVMavRo2Tu"] })
 	
 	expect(balance_obj.balance).toBeDefined()
 	done()
 }, 20000)
 
 test('Discover Chain on Account', (done) => {
-	var accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
+	let accountMaster = bip32.fromBase58("Fprv4xQSjQhWzrCVzvgkjam897LUV1AfxMuG8FBz5ouGAcbyiVcDYmqh7R2Fi22wjA56GQdmoU1AzfxsEmVnc5RfjGrWmAiqvfzmj4cCL3fJiiC", Networks.flo.network)
 	
-	var account = new Account(accountMaster, Networks.flo, {discover: false});
+	let account = new Account(accountMaster, Networks.flo, {discover: false});
 
 	account.discoverChain(0).then(function(acc){
 		expect(account.getChain(0).addresses.length).toBeGreaterThan(25)
@@ -184,9 +184,9 @@ test('Discover Chain on Account', (done) => {
 }, 20000)
 
 // test('Test send payment', (done) => {
-// 	var accountMaster = bip32.fromBase58("Fprt4gUYQvk1qawUgc4X6a5w3Qry67xXZEMwa1uKmfwMfWre1SP26Eaq1eEr9M9k29oc2qxChcstqqEDh6SWpnysXDeCuRAzyBGFXLi8ewVWrk3", Networks.flo_testnet.network)
+// 	let accountMaster = bip32.fromBase58("Fprt4gUYQvk1qawUgc4X6a5w3Qry67xXZEMwa1uKmfwMfWre1SP26Eaq1eEr9M9k29oc2qxChcstqqEDh6SWpnysXDeCuRAzyBGFXLi8ewVWrk3", Networks.flo_testnet.network)
 	
-// 	var account = new Account(accountMaster, Networks.flo_testnet, {discover: false});
+// 	let account = new Account(accountMaster, Networks.flo_testnet, {discover: false});
 
 // 	account.sendPayment({
 // 		to:  { oPHTT8kciUGjeqKrMYDHh4gL8DFBaNF1xL: 0.000001 },
