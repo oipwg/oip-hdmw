8b1051e97e7ebd662a1a65c107d320fcec886b01
orpheus <ryanachacon@gmail.com>
2019-04-23 10:35:23 -0700

    change testnet api to medici's testnet

--- src/networks/config.js
+++ src/networks/config.js
@@ -3,7 +3,7 @@ module.exports = {
     bitcoin: 'https://bitsight.failover.alexandria.io/api',
     bitcoin_testnet: 'https://bitsight.mk1.alexandria.io/api',
     flo: 'https://livenet.flocha.in/api',
-    flo_testnet: 'https://testnet.flocha.in/api',
+    flo_testnet: 'https://testnet.explorer.mediciland.com/api',
     litecoin: 'https://litesight.failover.alexandria.io/api',
     litecoin_testnet: 'https://litesight.mk1.alexandria.io/api'
   }
